f8849f04f2c9914aed4652a0117ba5c2a650f0c1
Jon Jagger <jon@jaggersoft.com>
2020-07-17 12:51:39 +0100

    Move to smaller Alpine base image

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,26 +1,11 @@
 # set the version to use:
-#NODE_VERSION=0.12.7
-#NODE_VERSION=4.1.1
-#NODE_VERSION=4.2.1
-#NODE_VERSION=6.11.1
-#NODE_VERSION=8.2.1
-#NODE_VERSION=8.4.0
-#NODE_VERSION=9.4.0
-#NODE_VERSION=9.10.1
-#NODE_VERSION=10.1.0
-#NODE_VERSION=13.7.0
-NODE_VERSION=14.1.0
 
-# Use npm package 'n' to call jshint and jasmine with selected node version:
+readonly MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 if [ -f .jshintrc ]; then
-  n use $NODE_VERSION \
-    /usr/lib/node_modules/jshint/bin/jshint \
-    --config .jshintrc \
-    *.js
+  jshint --config .jshintrc *.js
 fi
 
 if [ $? == 0 ]; then
-  n use $NODE_VERSION \
-    /usr/local/lib/node_modules/jasmine/bin/jasmine \
-    JASMINE_CONFIG_PATH=./jasmine.json
+  jasmine JASMINE_CONFIG_PATH="${MY_DIR}/jasmine.json"
 fi
