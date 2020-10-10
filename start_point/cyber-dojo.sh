7f5c92306f7f889918a1115a44b8625e982eaad7
Jon Jagger <jon@jaggersoft.com>
2020-10-10 12:33:49 +0100

    Add coverage to test run script

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,4 +1,15 @@
+# --------------------------------------------------------------
+# Text files under /sandbox are automatically returned...
+source ~/cyber_dojo_fs_cleaners.sh
+function cyber_dojo_exit()
+{
+  # Remove text files we don't want returned.
+  cyber_dojo_delete_dirs .nyc_output # ...
+  #cyber_dojo_delete_files ...
+}
+trap cyber_dojo_exit EXIT SIGTERM
 
+# --------------------------------------------------------------
 ln -s /etc/jasmine/node_modules ${CYBER_DOJO_SANDBOX}/node_modules
 
 npm run lint
